fc416f689135eb015ef68b51fc3fa40107d6af2e
Rafael Speranza de Souza <r.speranza@uni9.edu.br>
2025-09-02 00:02:10 +0000

    ./2025-MM-POO.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao();
+
+tv.Tamanho = 22.5f;
+
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
